13c74bb4502bd6a513add7060570ea442e96090a
Ryan O'Keeffe <okeefferd@gmail.com>
2015-09-09 15:03:17 -0500

    progress on allowing 'move' of volumes via snapshots

--- lib/AWSAttachVolume/main.rb
+++ lib/AWSAttachVolume/main.rb
@@ -7,27 +7,44 @@ module AWSAttachVolume
     include Methadone::ExitNow
 
     def initialize(options={})
-      @client = Aws::EC2::Client.new(region: options[:region])
-      @volume_id = options[:volume_id]
+      client= Aws::EC2::Client.new(region: options[:region])
+      @resource = Aws::EC2::Resource.new(client: client)
+      @volume = @resource.volume(options[:volume_id])
       @instance_id = options[:instance_id]
       @device = options[:device]
+      @move = options[:move]
 
       if @instance_id.nil? || @instance_id == ''
-        @instance_id = instance_id()
+        @instance_id = instance_id
       end
 
-      @instance_az = instance_az()
+      @instance_az = instance_az
     end
 
     def run()
-      volume_available(@client, @volume_id, @instance_az)
+
+      need_move = true if @volume.availability_zone != @instance_az
+
+      unless @volume.state == 'available'
+        fatal "Volume not in available state: #{@volume.state}"
+        exit_now!(-1)
+      end
+
+      unless !need_move || (need_move && @move)
+        fatal "Volume not in the same Availability Zone as the instance and --move not set.  Instance: #{@instance_az} Volume: #{@volume.availability_zone}"
+        exit_now!(-1)
+      end
+
+      if need_move && @move
+        # 'Move' the volume and return the id as the new volume_id
+        move_volume
+      end
 
       info "Attaching volume #{@volume_id} to instance #{@instance_id} as device #{@device}."
-      resp = @client.attach_volume({
-                                      volume_id: @volume_id,
-                                      instance_id: @instance_id,
-                                      device: @device
-                                  })
+      @volume.attach_to_instance({
+                                    instance_id: @instance_id,
+                                    device: @device
+                                })
       while(!File.exist?(@device))
         count = 0
         count += 1
@@ -52,22 +69,38 @@ module AWSAttachVolume
       return availability_zone
     end
 
-    def volume_available(client, volume_id, az)
-      resp = client.describe_volumes({
-                                         volume_ids: [volume_id],
-                                         max_results: 1
-                                     })
-      volume_az = resp.volumes[0].availability_zone
-      if volume_az != az
-        fatal "Volume and instance not in the same Availability Zone. Volume: #{volume_az} Instance: #{az}"
-        exit_now!(-1)
+    def hash_tags(hash)
+      tags = []
+      hash.each do |k,v|
+        tags << { key: k, value: v }
       end
-      state = resp.volumes[0].state
-      if state != 'available'
-        fatal "Volume not detached! Current state: #{state}"
-        exit_now!(-1)
-      end
-      return true
+    end
+
+    def move_volume()
+      # Snapshot volume and restore to current az.  Delete old volume & snapshot.
+      snapshot = @volume.create_snapshot({
+                                             description: 'AWSAttachVolume temp for moving volume'
+                                         })
+      snapshot.wait_until_completed
+      snapshot.create_tags({
+                             dry_run: true,
+                             tags: @volume.tags
+                         }) unless @volume.tags.nil? || @volume.tags == ''
+
+      new_volume = @resource.create_volume({
+                                    snapshot_id: snapshot.id,
+                                    availability_zone: @instance_az,
+                                    volume_type: 'gp2'
+                                  })
+      @resource.client.wait_until(:volume_available, {volume_ids: [new_volume.id]})
+      new_volume.create_tags({
+                                 dry_run: true,
+                                 tags: @volume.tags
+                             }) unless @volume.tags.nil? || @volume.tags == ''
+      @volume.delete
+      @resource.client.wait_until(:volume_deleted, {volume_ids: [@volume.id]})
+      @volume = new_volume
+      snapshot.delete
     end
   end
 end
